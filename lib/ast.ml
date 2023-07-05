5e8573e4a2c7e0ef58b139d946e5cb3b9c61ba9e
volesen <vincolesen@gmail.com>
2023-07-05 18:44:54 +0200

    Added `let` AST node

--- lib/ast.ml
+++ lib/ast.ml
@@ -2,6 +2,7 @@ type expr =
   | EInt of int
   | EBool of bool
   | EVar of string
+  | ELet of string * expr * expr
   | EIf of expr * expr * expr
   | EApp of expr * expr
   | EFun of string * expr

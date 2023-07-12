5c8faf55ab4fb0da6b6f4cf995ead49b9e0962de
volesen <vincolesen@gmail.com>
2023-07-12 12:28:40 +0200

    Added minimal binary operations

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,6 +1,12 @@
+type binop =
+  (* For now we skip `eq` as it requires typeclasses *)
+  | Add
+  | Lt
+
 type expr =
   | EInt of int
   | EBool of bool
+  | EBinop of binop * expr * expr
   | EVar of string
   | ELet of string * expr * expr
   | EIf of expr * expr * expr

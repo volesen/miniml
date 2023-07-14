cd64f789017d524ece55bc5edc2716d032ed2c1f
volesen <vincolesen@gmail.com>
2023-07-14 17:01:02 +0200

    Consistent op type name

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,6 +1,6 @@
 type un_op = Neg
 
-type binop =
+type bin_op =
   (* For now we skip `eq` as it requires typeclasses *)
   | Add
   | Sub
@@ -12,7 +12,7 @@ type expr =
   | EBool of bool
   | EVar of string
   | EUnOp of un_op * expr
-  | EBinOp of binop * expr * expr
+  | EBinOp of bin_op * expr * expr
   | ELet of string * expr * expr
   | EIf of expr * expr * expr
   | EApp of expr * expr

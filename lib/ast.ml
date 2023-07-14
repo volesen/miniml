35424a1262084d78dd4fe628be02ec0181079eaa
volesen <vincolesen@gmail.com>
2023-07-14 16:21:09 +0200

    Added unary operations

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,3 +1,5 @@
+type un_op = Neg
+
 type binop =
   (* For now we skip `eq` as it requires typeclasses *)
   | Add
@@ -9,7 +11,8 @@ type expr =
   | EInt of int
   | EBool of bool
   | EVar of string
-  | EBinop of binop * expr * expr
+  | EUnOp of un_op * expr
+  | EBinOp of binop * expr * expr
   | ELet of string * expr * expr
   | EIf of expr * expr * expr
   | EApp of expr * expr
